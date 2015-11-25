c7918db99b66b683072b6a77c8e1ddca64d2785f
ajongsma <a.jongsma@gmail.com>
2015-11-25 20:56:19 +0100

    Update osx_mouse_taptoclick_enable.sh

--- scrips/osx_mouse_taptoclick_enable.sh
+++ scrips/osx_mouse_taptoclick_enable.sh
@@ -4,7 +4,6 @@ echo "#-------------------------------------------------------------------------
 echo "# Enable Tap to Click for this user and for the login screen"
 echo "#------------------------------------------------------------------------------"
 
-source ../config.sh
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
