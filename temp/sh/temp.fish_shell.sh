9a87e950cd93ab6beb2ccefb13f7764608017196
ajongsma <a.jongsma@gmail.com>
2015-11-27 00:01:21 +0100

    Update temp.fish_shell.sh

--- temp/sh/temp.fish_shell.sh
+++ temp/sh/temp.fish_shell.sh
@@ -5,3 +5,17 @@ echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 
 # Change default shell to Fish
 chsh -s /usr/local/bin/fish
+
+# Create the Fish config directory
+mkdir -p ~/.config/fish
+
+# Create initial config file
+#vim ~/.config/fish/config.fish
+
+# add /usr/local/bin to the PATH environment variable
+#set -g -x PATH /usr/local/bin $PATH
+
+fish_config
+
+# Disable Fish welcome greeting
+#echo "set -g -x fish_greeting ''" >> ~/.config/fish/config.fish
