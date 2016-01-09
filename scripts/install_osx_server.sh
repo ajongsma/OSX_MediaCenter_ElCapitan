9ff4f60a50033687c5670c1005069c05b748e601
a.jongsma <a.jongsma@gmail.com>
2016-01-09 15:46:22 +0100

    .

--- scripts/install_osx_server.sh
+++ scripts/install_osx_server.sh
@@ -6,7 +6,7 @@ if [ -f _functions.sh ]; then
 elif [ -f ../_functions.sh ]; then
     source ../_functions.sh
 else
-   echo "Config file _functions.sh does not exist"
+   echo "Config file functions.sh does not exist"
    exit 1
 fi
 
@@ -22,6 +22,8 @@ fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 main() {
 	if ! cmd_exists 'serverinfo'; then
+		echo "OS X Server doesn't exists"
+
     if file_exists "$FOLDER_INSTALL/OS X Server 5.0.15.dmg"; then
       ask_for_sudo
       install_dmg "HOME/Downloads/${SABNZBD_DIR}-osx.dmg"
@@ -30,11 +32,12 @@ main() {
         ask_for_sudo
         cp -R "$FOLDER_INSTALL/Server.app" "/Applications/Server.app" 
       else
-        #echo "OS X Server App file NOT found in $FOLDER_INSTALL"
+        echo "OS X Server App file NOT found in $FOLDER_INSTALL"
+        exit 1
       fi
     fi    
 	fi
-	print_result $? 'OS X Server NOT found in $FOLDER_INSTALL"'
+	print_result $? 'OS X Server'
 }
 
 main
