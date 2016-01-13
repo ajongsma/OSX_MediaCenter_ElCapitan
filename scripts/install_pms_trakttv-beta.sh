51838c3f47354f0dca843fa01d49be5e0ab83df9
a.jongsma <a.jongsma@gmail.com>
2016-01-13 21:19:12 +0100

    ...

--- scripts/install_pms_trakttv-beta.sh
+++ scripts/install_pms_trakttv-beta.sh
@@ -20,29 +20,29 @@ else
 fi
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-## https://forums.plex.tv/discussion/193095/rel-hellohue-sync-and-control-your-philips-hue-lights-with-plex
+## https://forums.plex.tv/discussion/102818/rel-trakt
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
   ## Sanity check
-  if [ "${PLEX_PMS_HELLOHUE}" == "" ]; then
+  if [ "${PLEX_PMS_TRAKTTV}" == "" ]; then
     echo "Error: Not all config setting have been found set, please check config.sh."
     exit 1
   fi
 
   ## Let's do it
-  if ! folder_exists $PLEX_PMS_HELLOHUE; then
+  if ! folder_exists $PLEX_PMS_TRAKTTV; then
     ask_for_sudo
-    sudo mkdir -p $PLEX_PMS_HELLOHUE
-    sudo chown -R `whoami`:staff $PLEX_PMS_HELLOHUE
+    sudo mkdir -p $PLEX_PMS_TRAKTTV
+    sudo chown -R `whoami`:staff $PLEX_PMS_TRAKTTV
 
-    git clone https://github.com/ledge74/HelloHue.git $PLEX_PMS_HELLOHUE
-    print_result $? 'Download PMS channel HelloHue'
+    git clone -b beta https://github.com/trakt/Plex-Trakt-Scrobbler.git $PLEX_PMS_TRAKTTV
+    print_result $? 'Download PMS channel TraktTV'
 
-    sudo ln -s $PLEX_PMS_HELLOHUE $HOME/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/HelloHue.bundle
+    sudo ln -s $PLEX_PMS_TRAKTTV $HOME/Library/Application\ Support/Plex\ Media\ Server/Plug-ins/TraktTV-beta.bundle
   fi
 
-  print_result $? 'Plex PMS - HelloHue'
+  print_result $? 'Plex PMS - TraktTV'
 }
 
 main
