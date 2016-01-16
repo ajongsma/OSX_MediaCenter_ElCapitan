17851b7266243497e6d3811cdd5c53d217cc1c62
a.jongsma <a.jongsma@gmail.com>
2016-01-16 16:50:32 +0100

    ...

---
+++ scripts/install_nzbtomedia.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+## Initializing
+if [ -f _functions.sh ]; then
+  source _functions.sh
+elif [ -f ../_functions.sh ]; then
+  source ../_functions.sh
+else
+   echo "Config file functions.sh does not exist"
+   exit 1
+fi
+
+if [ -f config.sh ]; then
+  source config.sh
+elif [ -f ../config.sh ]; then
+  source ../config.sh
+else
+  echo "Config file config.sh does not exist"
+  exit 1
+fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+main() {
+  if ! folder_exists $NZBTOMEDIA_FOLDER; then
+    if [ "${NZBTOMEDIA_FOLDER}" == "" ] ; then
+      echo "Error: Not all config setting have been found set, please check config.sh."
+      exit 1
+    fi
+
+    ask_for_sudo
+    sudo mkdir -p $NZBTOMEDIA_FOLDER
+    sudo chown -R `whoami`:staff $NZBTOMEDIA_FOLDER
+
+    git clone git://github.com/midgetspy/Sick-Beard.git $NZBTOMEDIA_FOLDER
+    print_result $? 'Download NzbToMedia'
+  fi
+
+  print_result $? 'NzbToMedia'
+}
+
+main
