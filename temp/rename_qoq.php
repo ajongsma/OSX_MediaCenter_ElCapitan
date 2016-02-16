a740a09cdd8d892e8b02ae5a20d83623114e4155
ajongsma <a.jongsma@gmail.com>
2016-02-16 21:45:07 +0100

    Update rename_qoq.php

--- temp/rename_qoq.php
+++ temp/rename_qoq.php
@@ -86,7 +86,7 @@ if (preg_match('/(S|s)([0-9]+)(E|e)([0-9]+)/', $title)) {
     $cleanshowname2 = trim(str_replace('(2015)','',$cleanshowname));
     doLog("Clean Show Name 2: " . $cleanshowname2);
     
-    $tvdb_series_info = get_tvdb_seriesinfo($cleanshowname);
+    $tvdb_series_info = get_tvdb_seriesinfo($cleanshowname2);
     if ($tvdb_series_info === false) {
             doLog("--> TVDB Status: False");
     }
